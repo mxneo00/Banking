c653c5613b10635fe18778edc0cee79a47ecd2c6
LumpyTacos <kevinlam718@gmail.com>
2026-08-11 11:27:28 -0400

    removed branch controller in main

--- app/main.py
+++ app/main.py
@@ -14,10 +14,9 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from controllers.account_controller import router as account_router
-from controllers.branch_controller import router as branch_router
+from controllers.accountController import router as account_router
 from controllers.customerController import router as customer_router
-from controllers.transaction_controller import router as transaction_router
+from app.controllers.transactionController import router as transaction_router
 
 from models.exceptions import (
     BankingError,
